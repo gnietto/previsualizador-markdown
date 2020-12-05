665effa832f72dc639745f68fe6addce8eb9fa8a
workstation <elcirculoti@gmail.com>
2020-12-05 02:20:15 -0300

    Updates

---
+++ precache-manifest.7ad7263dfedc83f0ac61b7ae9b036e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7dedcfce0df801448e6350acdaed59",
+    "url": "/previsualizador-markdown/index.html"
+  },
+  {
+    "revision": "05762c4c8b05ff6d1c2f",
+    "url": "/previsualizador-markdown/static/css/main.1805e759.chunk.css"
+  },
+  {
+    "revision": "3af6af0419bb6480a7be",
+    "url": "/previsualizador-markdown/static/js/2.e42e1cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/previsualizador-markdown/static/js/2.e42e1cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05762c4c8b05ff6d1c2f",
+    "url": "/previsualizador-markdown/static/js/main.7bc83b6f.chunk.js"
+  },
+  {
+    "revision": "3bd22977ff5588019067",
+    "url": "/previsualizador-markdown/static/js/runtime-main.a7f6aadc.js"
+  }
+]);
